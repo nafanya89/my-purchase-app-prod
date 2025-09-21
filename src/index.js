82d11808ef7ae780124f3c545ba7bf63f7e44da5
Vasyl Bodnaruk <vasylbodnaruk@Vasyls-MacBook-Air.local>
2025-09-21 20:31:56 +0300

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,5 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorkerRegistration.register();
